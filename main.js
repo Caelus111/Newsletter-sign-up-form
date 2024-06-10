8f97168899ffc5cde8b9df20b4ae2739d1bf69c6
caelus111 <wmaarouf660@gmail.com>
2024-06-10 16:49:37 +0100

    Newsletter login Component

---
+++ main.js
@@ -0,0 +1,36 @@
+const form = document.querySelector("form");
+const emailInput = document.querySelector("#email");
+const submitBtn = document.querySelector("form button");
+const errorText = document.querySelector("form p");
+
+const container = document.querySelector(".container");
+const confirmation = document.querySelector(".confirmation");
+
+function validateEmail(e) {
+  e.preventDefault();
+
+  const valEmail = /^\S+@\S+\.\S+$/;
+
+  if (!valEmail.test(emailInput.value)) {
+    emailInput.style.borderColor = "hsl(4, 100%, 67%)";
+    emailInput.classList.add("error");
+    errorText.style.display = "block";
+  } else {
+    emailInput.classList.remove("error");
+    errorText.style.display = "none";
+
+    container.style.display = "none";
+    confirmation.style.display = "flex";
+  }
+}
+
+emailInput.addEventListener("focus", (e) => {
+    emailInput.style.borderColor = "hsl(234, 29%, 20%)";
+});
+
+confirmation.querySelector("button").addEventListener("click", (e) => {
+  confirmation.style.display = "none";
+  container.style.display = "flex";
+});
+
+form.addEventListener("submit", validateEmail);
